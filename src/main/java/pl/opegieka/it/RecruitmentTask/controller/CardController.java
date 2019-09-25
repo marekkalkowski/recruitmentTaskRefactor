a66361ed5c9cc1c308b7eb7971f81f4990635e49
kalkowski.m <dj.marek@wp.pl>
2019-09-25 15:24:16 +0200

    log2j2 added to CardController.class

--- src/main/java/pl/opegieka/it/RecruitmentTask/controller/CardController.java
+++ src/main/java/pl/opegieka/it/RecruitmentTask/controller/CardController.java
@@ -13,7 +13,6 @@ import pl.opegieka.it.RecruitmentTask.service.RegexService;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping(value = "/api/cards")
 public class CardController {
@@ -21,47 +20,74 @@ public class CardController {
     private static final Logger LOG =
             LoggerFactory.getLogger(CardController.class);
 
-
     @Autowired
     private CardDao cardDao;
 
     @Autowired
-    RegexService regexService;
+    private RegexService regexService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Card> getAllCards() {
         LOG.info("Zapytanie GET do /api/cards; metoda getAllCards ");
-        return cardDao.findAll();
+
+        List<Card> cardList = cardDao.findAll();
+
+        LOG.info("Zrócono wszystkie karty w liczbie : {} : ",cardList.size());
+
+        return cardList;
     }
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Card addCard(@RequestBody Card card) {
         LOG.info("Zapytanie POST do /api/cards; metoda addCard ");
+        LOG.debug("Sprawdzam czy numer karty jest liczbą całkowitą .......");
 
         checkCardNumberLenght(card);
 
+        LOG.debug("Sprawdzenie czy numer karty na obiekcie Card jest liczbą całkowitą: udane ");
+        LOG.debug("Pobranie numer karty z obiektu Card");
+
         int cardNumber = card.getCardNumber();
+
+        LOG.info("Pobrano numer karty: {}", cardNumber);
+        LOG.debug("Sprawdzenie czy karta o podanym numer {} istnieje", cardNumber);
+
         if (cardDao.checkIfExist(cardNumber) == 1) {
+            LOG.warn("Card with nuber {} already exist", cardNumber);
             throw new AllreadyExistException(String.format("Card with nuber %s already exist", cardNumber));
         }
 
+        LOG.debug("Zapisywanie nowej karty .... ");
         cardDao.save(card);
 
         LOG.info("Zapisano nową kartę" + card);
-        return card;
 
+        return card;
     }
 
 
     @GetMapping(value = "/{cardId}")
     public Card getCardById(@PathVariable("cardId") String cardId) {
 
+        LOG.info("Wyszukiwanie karty po id");
+        LOG.info("Zapytanie GET do /api/cards/{cardId}; metoda getCardById ");
+        LOG.debug("Sprawdzam czy id karty jest liczbą całkowitą .......");
+
         checkFormatOfCardId(cardId);
+
+        LOG.debug("Sprawdzenie czy id karty na obiekcie Card jest liczbą całkowitą: udane ");
 
         long checkedCardId = Long.valueOf(cardId);
 
+        LOG.debug("Ppobieranie obiektu Card z bazy danych o id: {}", checkedCardId);
+
         Card card = cardDao.findById(checkedCardId);
+
+        LOG.info("Pobrano obiekt Card z bazy danych o id: {} : . Karta: {}", checkedCardId, card);
+        LOG.debug("Sprawdzenie czy obiekt o numerze karty {} istnieje ", checkedCardId);
+
         if (card == null) {
+            LOG.warn("Karta o numerze {} nie istnieje w bazie" + NotFoundException.class);
             throw new NotFoundException("Card id not found");
         }
         return card;
@@ -84,12 +110,14 @@ public class CardController {
 
     private void checkFormatOfCardId(String cardId) {
         if (!regexService.checkRegex(cardId)) {
+            LOG.warn("Id karty nie jest liczba całkowitą " + NumberFormatException.class);
             throw new NumberFormatException("Card id must be integer!");
         }
     }
 
     private void checkCardNumberLenght(Card card) {
         if (Integer.valueOf(card.getCardNumber()).toString().length() > 8) {
+            LOG.warn("Numer karty nie jest liczba całkowitą " + NumberFormatException.class);
             throw new NumberFormatException("Card number to long. Has to be max 8 digits.");
         }
     }
